5fce907ad401d1e6472798aa5d06b3345c9ef1e7
skfro266 <skfro266@naver.com>
2018-12-10 17:46:02 +0900

    20181210_04

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,154 @@
+
+var data = [{
+    datasets: [{
+        data: [95],
+        backgroundColor: [
+            '#67c2ff'
+        ],
+        hoverBackgroundColor:[
+            '#0031bb'
+        ]
+    }]
+},{
+    datasets: [{
+        data: [85],
+        backgroundColor: [
+            '#67c2ff'
+        ],
+        hoverBackgroundColor:[
+            '#0031bb'
+        ]
+    }]
+},{
+    datasets: [{
+        data: [85],
+        backgroundColor: [
+            '#67c2ff'
+        ],
+        hoverBackgroundColor:[
+            '#0031bb'
+        ]
+    }]
+},{
+    datasets: [{
+        data: [50],
+        backgroundColor: [
+            '#67c2ff'
+        ],
+        hoverBackgroundColor:[
+            '#0031bb'
+        ]
+    }]
+},{
+    datasets: [{
+        data: [50],
+        backgroundColor: [
+            '#67c2ff'
+        ],
+        hoverBackgroundColor:[
+            '#0031bb'
+        ]
+    }]
+}];
+var option = [{
+    cutoutPercentage: 40,//그래프의 두께
+    rotation: -0.5 * Math.PI,//돌아가는 시작점
+    circumference: 	1.9 * Math.PI,//차지하는 영역(지름)
+    animation: {
+        animateScale: true//나타날때 돌면서 나타남
+    },
+    title: {
+        display: true,
+        text: 'Photoshop/Illustrator',
+        fontSize: 16,
+        fontColor: "#666"
+    },
+    legend: {
+        display: false
+    }, tooltips :{
+        enabled: false
+    }
+},{
+    cutoutPercentage: 40,//그래프의 두께
+    rotation: -0.5 * Math.PI,//돌아가는 시작점
+    circumference: 	1.7 * Math.PI,//차지하는 영역(지름)
+    animation: {
+        animateScale: true//나타날때 돌면서 나타남
+    },
+    title: {
+        display: true,
+        text: 'Html',
+        fontSize: 16,
+        fontColor: "#666"
+    },
+    legend: {
+        display: false
+    }, tooltips :{
+        enabled: false
+    }
+},{
+    cutoutPercentage: 40,//그래프의 두께
+    rotation: -0.5 * Math.PI,//돌아가는 시작점
+    circumference: 	1.7 * Math.PI,//차지하는 영역(지름)
+    animation: {
+        animateScale: true//나타날때 돌면서 나타남
+    },
+    title: {
+        display: true,
+        text: 'CSS',
+        fontSize: 16,
+        fontColor: "#666"
+    },
+    legend: {
+        display: false
+    }, tooltips :{
+        enabled: false
+    }
+},{
+    cutoutPercentage: 40,//그래프의 두께
+    rotation: -0.5 * Math.PI,//돌아가는 시작점
+    circumference: 	1 * Math.PI,//차지하는 영역(지름)
+    animation: {
+        animateScale: true//나타날때 돌면서 나타남
+    },
+    title: {
+        display: true,
+        text: 'Javascript',
+        fontSize: 16,
+        fontColor: "#666"
+    },
+    legend: {
+        display: false
+    }, tooltips :{
+        enabled: false
+    }
+},{
+    cutoutPercentage: 40,//그래프의 두께
+    rotation: -0.5 * Math.PI,//돌아가는 시작점
+    circumference: 	1 * Math.PI,//차지하는 영역(지름)
+    animation: {
+        animateScale: true//나타날때 돌면서 나타남
+    },
+    title: {
+        display: true,
+        text: 'jQuery',
+        fontSize: 16,
+        fontColor: "#666"
+    },
+    legend: {
+        display: false
+    }, tooltips :{
+        enabled: false
+    }
+}];
+
+
+
 var scTop = 0;
 var navChk = true;
 var page = [];
 var nowPage = 0;
-
+var chartChk = true;
 $(window).scroll(function(){
 	scTop = $(window).scrollTop();
 	if(scTop > 150) {
@@ -10,7 +156,21 @@ $(window).scroll(function(){
 	}
 	else {
 		$(".sc_navs").stop().animate({"left":"-15%"}, 100);
-	}
+    }
+    if($("html,body").scrollTop() > $(".skill").offset().top - 200) {
+        if(chartChk) {
+            var chart = [];
+            var ctx = $(".chart");
+            ctx.each(function(i){
+                chart[i] = new Chart($(this),{
+                    type: 'doughnut',
+                    data: data[i],
+                    options: option[i]
+                })
+            });
+            chartChk = false;
+        }
+    }
 });
 
 
@@ -29,135 +189,5 @@ $(".navs > li, .sc_navs > li").click(function(){
 });
 
 
-var data = [{
-    datasets: [{
-        data: [95, 5],
-        backgroundColor: [
-            '#67c2ff',
-            '#fff'
-        ],
-        hoverBackgroundColor:[
-            '#0031bb'
-        ]
-    }]
-},{
-    datasets: [{
-        data: [85, 15],
-        backgroundColor: [
-            '#67c2ff',
-            '#fff'
-        ],
-        hoverBackgroundColor:[
-            '#0031bb'
-        ]
-    }]
-},{
-    datasets: [{
-        data: [85, 15],
-        backgroundColor: [
-            '#67c2ff',
-            '#fff'
-        ],
-        hoverBackgroundColor:[
-            '#0031bb'
-        ]
-    }]
-},{
-    datasets: [{
-        data: [50, 50],
-        backgroundColor: [
-            '#67c2ff',
-            '#fff'
-        ],
-        hoverBackgroundColor:[
-            '#0031bb'
-        ]
-    }]
-},{
-    datasets: [{
-        data: [50, 50],
-        backgroundColor: [
-            '#67c2ff',
-            '#fff'
-        ],
-        hoverBackgroundColor:[
-            '#0031bb'
-        ]
-    }]
-}];
-var option = [{
-    cutoutPercentage: 40,//그래프의 두께
-    rotation: -0.5 * Math.PI,//돌아가는 시작점
-    circumference: 	2 * Math.PI,//차지하는 영역(지름)
-    animation: {
-        animateScale: true//나타날때 돌면서 나타남
-    },
-    title: {
-        display: true,
-        text: 'Photoshop/Illustrator',
-        fontSize: 16,
-        fontColor: "#666"
-    }
-},{
-    cutoutPercentage: 40,//그래프의 두께
-    rotation: -0.5 * Math.PI,//돌아가는 시작점
-    circumference: 	2 * Math.PI,//차지하는 영역(지름)
-    animation: {
-        animateScale: true//나타날때 돌면서 나타남
-    },
-    title: {
-        display: true,
-        text: 'Html',
-        fontSize: 16,
-        fontColor: "#666"
-    }
-},{
-    cutoutPercentage: 40,//그래프의 두께
-    rotation: -0.5 * Math.PI,//돌아가는 시작점
-    circumference: 	2 * Math.PI,//차지하는 영역(지름)
-    animation: {
-        animateScale: true//나타날때 돌면서 나타남
-    },
-    title: {
-        display: true,
-        text: 'CSS',
-        fontSize: 16,
-        fontColor: "#666"
-    }
-},{
-    cutoutPercentage: 40,//그래프의 두께
-    rotation: -0.5 * Math.PI,//돌아가는 시작점
-    circumference: 	2 * Math.PI,//차지하는 영역(지름)
-    animation: {
-        animateScale: true//나타날때 돌면서 나타남
-    },
-    title: {
-        display: true,
-        text: 'Javascript',
-        fontSize: 16,
-        fontColor: "#666"
-    }
-},{
-    cutoutPercentage: 40,//그래프의 두께
-    rotation: -0.5 * Math.PI,//돌아가는 시작점
-    circumference: 	2 * Math.PI,//차지하는 영역(지름)
-    animation: {
-        animateScale: true//나타날때 돌면서 나타남
-    },
-    title: {
-        display: true,
-        text: 'jQuery',
-        fontSize: 16,
-        fontColor: "#666"
-    }
-}];
-var chart = [];
 
-var ctx = $(".chart");
-ctx.each(function(i){
-    chart[i] = new Chart($(this),{
-        type: 'doughnut',
-        data: data[i],
-        options: option[i]
-    })
-});
+
